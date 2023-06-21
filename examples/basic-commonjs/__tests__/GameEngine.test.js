e478f6535884b5a8e9ad29355aef28901dd37b5a
GJ Tiquia <gershom.tiquia@gmail.com>
2023-06-21 18:38:53 +0800

    feat: updated tests in basic examples to test GameEngine in different environments

---
+++ examples/basic-commonjs/__tests__/GameEngine.test.js
@@ -0,0 +1,40 @@
+const { GameEngine, defaultConfig } = require("@headless-game-engine/core");
+
+describe("GameEngine", () => {
+    it("should initialize correctly", () => {
+        const gameEngine = new GameEngine(defaultConfig);
+
+        expect(gameEngine.tick).toStrictEqual(0);
+    })
+
+    it("should tick correctly", () => {
+        const gameEngine = new GameEngine(defaultConfig);
+
+        expect(gameEngine.tick).toStrictEqual(0);
+
+        gameEngine.fixedUpdate();
+        expect(gameEngine.tick).toStrictEqual(1);
+
+        gameEngine.fixedUpdate();
+        expect(gameEngine.tick).toStrictEqual(2);
+
+        gameEngine.fixedUpdate();
+        expect(gameEngine.tick).toStrictEqual(3);
+    })
+
+    it("should reinitialize correctly", () => {
+        const gameEngine = new GameEngine(defaultConfig);
+
+        expect(gameEngine.tick).toStrictEqual(0);
+
+        gameEngine.fixedUpdate();
+        expect(gameEngine.tick).toStrictEqual(1);
+
+        gameEngine.initialize();
+
+        expect(gameEngine.tick).toStrictEqual(0);
+
+        gameEngine.fixedUpdate();
+        expect(gameEngine.tick).toStrictEqual(1);
+    })
+})
