ba7a7f49817e0926bb62bd4586ef4bedbb37742b
GJ Tiquia <gershom.tiquia@gmail.com>
2023-06-27 01:01:36 +0800

    feat: added jump mechanic in basic typescript example

--- examples/basic-typescript/src/index.ts
+++ examples/basic-typescript/src/index.ts
@@ -5,39 +5,27 @@ import { EngineClock, Clock, Time, sleep } from "@headless-game-engine/clock";
 
 import * as readline from "readline"
 
-const SCREEN_WIDTH = 80;
+const SCREEN_WIDTH = 30;
 const REFRESH_RATE = 60;
-const TOTAL_RUNTIME = 10; // seconds
+const MAX_RUNTIME = 300; // seconds
 
 const TICK_RATE = 40;
 
 const main = async () => {
-    readline.emitKeypressEvents(process.stdin);
+    EngineClock.start(gameEngine);
+    renderClock.start();
 
-    process.stdin.setRawMode(true);
-    process.stdin.on('keypress', (str: string, key: readline.Key) => {
+    // TODO : Refactor
+    startListeningToKeypress();
 
-        console.log(str, key);
+    await sleep(MAX_RUNTIME * 1000);
+    console.clear();
 
-        // "Raw" mode so we must do our own kill switch
-        if (key.ctrl && key.name === "c") {
-            process.exit();
-        }
+    stopListeningToKeypress();
+    EngineClock.stop();
+    renderClock.stop();
 
-        // User has triggered a keypress, now do whatever we want!
-        // ...
-
-    });
-
-
-    // EngineClock.start(gameEngine);
-    // renderClock.start();
-
-    // await sleep(TOTAL_RUNTIME * 1000);
-    // console.clear();
-
-    // EngineClock.stop();
-    // renderClock.stop();
+    quit();
 }
 
 class MovingPoint extends Component {
@@ -57,11 +45,15 @@ class MovingPoint extends Component {
 
         this.transform.position = currentPosition;
     }
+
+    public jump(): void {
+        this._velocity = 50;
+    }
 }
 
 const movingPointPrefab: GameObjectConfig = {
     name: "MovingPoint",
-    transform: { position: { x: SCREEN_WIDTH, y: 0, z: 0 } },
+    transform: { position: { x: 0, y: 0, z: 0 } },
     components: [{ component: MovingPoint }]
 }
 
@@ -76,19 +68,57 @@ const movingPointInstance = gameEngine.findGameObjectByName(movingPointPrefab.na
 if (!movingPointInstance)
     throw new Error(`Cannot find game object with name ${movingPointPrefab.name}!`)
 
+const movingPointComponent = movingPointInstance.getComponent(MovingPoint);
+if (!movingPointComponent)
+    throw new Error(`Cannot find component with class ${MovingPoint.name}!`)
+
 const renderClock = new Clock(() => render(), 1000 / REFRESH_RATE);
 
 const render = () => {
     // TODO : Interpolation
     const position = Math.round(movingPointInstance.transform.position.x);
 
-    const pointGraphic = "0";
-    const leftSpace = " ".repeat(position);
-    const rightSpace = " ".repeat(SCREEN_WIDTH - position);
+    const pointGraphic = position > 0 ? "🏀" : "🏀";
+    const leftSpace = " ".repeat(Math.max(0, position));
+    const rightSpace = " ".repeat(Math.max(0, SCREEN_WIDTH - position));
 
     console.clear();
     console.log(`Refresh Rate: ${REFRESH_RATE}, Tick Rate: ${TICK_RATE}, Ticks: ${gameEngine.tick}`);
-    console.log(`|${leftSpace}${pointGraphic}${rightSpace}|`)
+    console.log(`\n➡️|${leftSpace}${pointGraphic}${rightSpace}|`)
+    console.log("\nHeadless Game Engine Demo");
+    console.log("\nPress Space to Jump.");
+    console.log("Press Ctrl-C to Quit.");
+}
+
+function startListeningToKeypress() {
+    readline.emitKeypressEvents(process.stdin);
+
+    process.stdin.setRawMode(true);
+    process.stdin.on('keypress', keypressEventHandler);
+}
+
+function stopListeningToKeypress() {
+    process.stdin.setRawMode(false);
+    process.stdin.removeListener('keypress', keypressEventHandler);
+}
+
+function quit() {
+    process.exit();
+}
+
+function keypressEventHandler(str: string, key: readline.Key) {
+    // "Raw" mode so we must do our own kill switch
+    if (key.ctrl && key.name === "c") {
+        process.exit();
+    }
+
+    if (key.name === "space") {
+        onJumpPressed();
+    }
+}
+
+function onJumpPressed() {
+    movingPointComponent?.jump();
 }
 
 main();
