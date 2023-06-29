b23bcd8c5dc9c6372407ad29f5e49835e6633a6a
GJ Tiquia <gershom.tiquia@gmail.com>
2023-06-29 18:21:15 +0800

    feat: input handling and movement parameter tweaks

--- examples/platformer-cli/src/assets/scripts/PlayerAgent.ts
+++ examples/platformer-cli/src/assets/scripts/PlayerAgent.ts
@@ -1,8 +1,17 @@
 import { Time } from "@headless-game-engine/clock";
 import { Component, Vector2 } from "@headless-game-engine/core";
 
+const GRAVITY = -80;
+const GRAVITY_MULTIPLIER = 2.5;
+
+const JUMP_VELOCITY = 35;
+const MAX_FALL_VELOCITY = -30;
+
+const MOVE_ACCELERATION = 100;
+const MAX_MOVE_VELOCITY = 40;
+
 export class PlayerAgent extends Component {
-    private _acceleration: Vector2 = { x: 0, y: -20 };
+    private _acceleration: Vector2 = { x: 0, y: GRAVITY };
     private _velocity: Vector2 = { x: 0, y: 0 };
 
     public override fixedUpdate(): void {
@@ -10,8 +19,19 @@ export class PlayerAgent extends Component {
 
         const currentPosition = this.transform.position;
 
+        if (this._velocity.y <= 0)
+            this._acceleration.y = GRAVITY_MULTIPLIER * GRAVITY;
+        else
+            this._acceleration.y = GRAVITY;
+
         this._velocity.x += this._acceleration.x * Time.fixedDeltaTime;
         this._velocity.y += this._acceleration.y * Time.fixedDeltaTime;
+
+        if (Math.abs(this._velocity.x) > MAX_MOVE_VELOCITY)
+            this._velocity.x = (this._velocity.x / Math.abs(this._velocity.x)) * MAX_MOVE_VELOCITY;
+
+        if (this._velocity.y < MAX_FALL_VELOCITY)
+            this._velocity.y = MAX_FALL_VELOCITY;
 
         currentPosition.x += this._velocity.x * Time.fixedDeltaTime;
         currentPosition.y += this._velocity.y * Time.fixedDeltaTime;
@@ -22,5 +42,22 @@ export class PlayerAgent extends Component {
         }
 
         this.transform.position = currentPosition;
+    }
+
+    public jump() {
+        this._velocity.y = JUMP_VELOCITY;
+    }
+
+    public moveRight() {
+        this._acceleration.x = MOVE_ACCELERATION;
+    }
+
+    public moveLeft() {
+        this._acceleration.x = -1 * MOVE_ACCELERATION;
+    }
+
+    public stop() {
+        this._acceleration.x = 0;
+        this._velocity.x = 0;
     }
 }
--- examples/platformer-cli/src/main.ts
+++ examples/platformer-cli/src/main.ts
@@ -1,8 +1,10 @@
 import { EngineClock, sleep } from "@headless-game-engine/clock";
 import { Vector2 } from "@headless-game-engine/core";
 
+import * as readline from "readline"
+
 import { gameEngine } from "./gameEngine.config.js";
-import { RectRenderer } from "./assets/index.js";
+import { PlayerAgent, RectRenderer } from "./assets/index.js";
 import { RenderClock } from "./RenderClock.js";
 
 const SCREEN_SIZE: Vector2 = { x: 80, y: 20 }
@@ -12,14 +14,56 @@ const basePlatform = gameEngine.findGameObjectByName("Base Platform");
 const baseRectRenderer = basePlatform?.getComponent(RectRenderer);
 baseRectRenderer?.setSize({ x: SCREEN_SIZE.x, y: 1 })
 
+const player = gameEngine.findGameObjectByName("Player");
+const playerAgent = player?.getComponent(PlayerAgent);
+
 export async function main() {
     EngineClock.start(gameEngine);
     RenderClock.start(gameEngine, SCREEN_SIZE, REFRESH_RATE)
+    startListeningToKeypress();
 
     // console.clear();
     // process.exit();
 }
 
+function startListeningToKeypress() {
+    readline.emitKeypressEvents(process.stdin);
+
+    process.stdin.setRawMode(true);
+    process.stdin.on('keypress', keypressEventHandler);
+}
+
+function stopListeningToKeypress() {
+    process.stdin.setRawMode(false);
+    process.stdin.removeListener('keypress', keypressEventHandler);
+}
+
+function quit() {
+    process.exit();
+}
+
+function keypressEventHandler(str: string, key: readline.Key) {
+    // "Raw" mode so we must do our own kill switch
+    if (key.ctrl && key.name === "c") {
+        process.exit();
+    }
+
+    if (key.name === "space" || key.name === "w" || key.name === "up") {
+        playerAgent?.jump();
+    }
+
+    if (key.name === "a" || key.name === "left") {
+        playerAgent?.moveLeft();
+    }
+
+    if (key.name === "d" || key.name === "right") {
+        playerAgent?.moveRight();
+    }
+
+    if (key.name === "s" || key.name === "down") {
+        playerAgent?.stop();
+    }
+}
 
 
 
