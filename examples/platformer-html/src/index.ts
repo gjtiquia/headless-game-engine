71d8bac95d6817aaca9125f27927a04931ced760
GJ Tiquia <gershom.tiquia@gmail.com>
2023-07-05 11:06:48 +0800

    feat: basic html canvas

--- examples/platformer-html/src/index.ts
+++ examples/platformer-html/src/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+console.log("Script Loaded!");
+
+// const WIDTH = window.innerWidth;
+// const HEIGHT = window.innerHeight;
+const WIDTH = 500;
+const HEIGHT = 500;
+
+const canvas: HTMLCanvasElement = document.getElementById("gameCanvas") as HTMLCanvasElement;
+canvas.width = WIDTH;
+canvas.height = HEIGHT;
+
+const context: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
+context.fillStyle = "black"
+context.fillRect(0, 0, WIDTH, HEIGHT);
+
+context.fillStyle = "white"
+
+context.fillRect(0, 0, WIDTH / 2, HEIGHT / 2);
+context.fillRect(WIDTH / 2, HEIGHT / 2, WIDTH / 2, HEIGHT / 2);
